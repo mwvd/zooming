49b2599c57a59563033cf1e1c0cc3f56ac666a02
Mackenzie Davidson <mwvdavidson@gmail.com>
2021-03-08 02:27:31 -0500

    zooming-overlay class

--- src/overlay.js
+++ src/overlay.js
@@ -1,48 +1,49 @@
-import { listen, setStyle } from './utils'
+import { listen, setStyle } from "./utils";
 
 export default {
-  init(instance) {
-    this.el = document.createElement('div')
-    this.instance = instance
-    this.parent = document.body
+    init(instance) {
+        this.el = document.createElement("div");
+        this.el.className = "zoom-overlay";
+        this.instance = instance;
+        this.parent = document.body;
 
-    setStyle(this.el, {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      opacity: 0
-    })
+        setStyle(this.el, {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            opacity: 0,
+        });
 
-    this.updateStyle(instance.options)
-    listen(this.el, 'click', instance.handler.clickOverlay.bind(instance))
-  },
+        this.updateStyle(instance.options);
+        listen(this.el, "click", instance.handler.clickOverlay.bind(instance));
+    },
 
-  updateStyle(options) {
-    setStyle(this.el, {
-      zIndex: options.zIndex,
-      backgroundColor: options.bgColor,
-      transition: `opacity
+    updateStyle(options) {
+        setStyle(this.el, {
+            zIndex: options.zIndex,
+            backgroundColor: options.bgColor,
+            transition: `opacity
         ${options.transitionDuration}s
-        ${options.transitionTimingFunction}`
-    })
-  },
+        ${options.transitionTimingFunction}`,
+        });
+    },
 
-  insert() {
-    this.parent.appendChild(this.el)
-  },
+    insert() {
+        this.parent.appendChild(this.el);
+    },
 
-  remove() {
-    this.parent.removeChild(this.el)
-  },
+    remove() {
+        this.parent.removeChild(this.el);
+    },
 
-  fadeIn() {
-    this.el.offsetWidth
-    this.el.style.opacity = this.instance.options.bgOpacity
-  },
+    fadeIn() {
+        this.el.offsetWidth;
+        this.el.style.opacity = this.instance.options.bgOpacity;
+    },
 
-  fadeOut() {
-    this.el.style.opacity = 0
-  }
-}
+    fadeOut() {
+        this.el.style.opacity = 0;
+    },
+};
